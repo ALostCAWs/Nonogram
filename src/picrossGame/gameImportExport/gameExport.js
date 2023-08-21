33bf7098ff4074fd89663bbe6cb96537f08cb99a
ALostCAWs <keighly@keighly.ca>
2023-08-20 21:49:32 -0400

    Adds basic components to build create mode in

--- src/picrossGame/gameImportExport/gameExport.js
+++ src/picrossGame/gameImportExport/gameExport.js
@@ -1,4 +1,36 @@
-export const encodeGameHAsh = (gameSolution) => {
+/* ---- Imports Section */
+import React, { useState, useEffect } from 'react';
+import { PicrossProvider } from '../picrossProvider';
+
+/* ---- Create Game by providing user with a blank board & allowing them to toggle tile fillState.filled */
+// Call exportGame on submit
+export const CreateGame = () => {
+  const [puzzleCode, setPuzzleCode] = useState('');
+  const [submit, setSubmit] = useState(false);
+
+  // useEffect trigger on first render
+  useEffect(() => {
+    // Create blank board ( fillState.empty values only ) based on height / width chosen
+  }, []);
+
+  const boardHeight = 5;
+  const boardWidth = 5;
+
+  /* <- Handle Puzzle Submission -> */
+  const handleSubmit = (e) => {
+    setPuzzleCode(exportGame(puzzleCode));
+    setSubmit(true);
+    // Take user to page where they can copy their puzzles' code after exporting
+    // Include copy button
+  }
+
+  return (
+    <>
+    </>
+  );
+}
+
+export const exportGame = (gameSolution) => {
   // First item is separated from the rest of the string with a pipe (|), this is the boards' width
   let gameHash = `${String(gameSolution[0].length)}|`;
 
