31c6749fc87bb7ba63cae86c1adb9f1abf633ee4
ALostCAWs <keighly@keighly.ca>
2023-12-07 20:24:16 -0500

    Switched currentPuzzle in Create to useReducer

--- src/components/providers/createNonogramProvider.tsx
+++ src/components/providers/createNonogramProvider.tsx
@@ -1,14 +1,15 @@
 /* ---- Imports Section */
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import App from 'App';
 // Constants
+import { PUZZLE_ACTIONS } from 'constants/puzzleActions';
 import { FILL_STATE } from "constants/fillState";
 // Components > UI
 import { Board } from 'components/ui/board';
 // Functions
 import { hoverTile } from 'functions/tileFunctions';
 import { checkBoardNotBlank } from 'functions/puzzleValidation';
-import { createBlankPuzzle, createBoolPuzzle } from 'functions/puzzleSetup';
+import { createBlankPuzzle, createBoolPuzzle, copyCurrentPuzzle } from 'functions/puzzleSetup';
 /* End ---- */
 
 interface CreateNonogramProviderProps {
@@ -24,37 +25,48 @@ interface CreateNonogramProviderProps {
  * @returns Loads the home screen when the puzzle is exported
  */
 export const CreateNonogramProvider = ({ boardHeight, boardWidth }: CreateNonogramProviderProps) => {
-  const [currentPuzzle, setCurrentPuzzle] = useState<string[][]>(createBlankPuzzle(boardHeight, boardWidth));
+  //const [currentPuzzle, setCurrentPuzzle] = useState<string[][]>(createBlankPuzzle(boardHeight, boardWidth));
   const [submit, setSubmit] = useState<boolean>(false);
   const [boardBlank, setBoardBlank] = useState<boolean>(true);
+
+  /* ---- useReducer */
+  const [currentPuzzle, currentPuzzleDispatch] = useReducer(currentPuzzleReducer, createBlankPuzzle(boardHeight, boardWidth));
+
+  interface PuzzleAction {
+    type: string,
+    rowIndex: number,
+    colIndex: number
+  }
+
+  function currentPuzzleReducer(puzzleState: string[][], action: PuzzleAction): string[][] {
+    switch (action.type) {
+
+      case PUZZLE_ACTIONS.FILL: {
+        const updatedPuzzle = copyCurrentPuzzle(puzzleState);
+        updatedPuzzle[action.rowIndex][action.colIndex] = updatedPuzzle[action.rowIndex][action.colIndex] === FILL_STATE.EMPTY ? FILL_STATE.FILLED : FILL_STATE.EMPTY;
+        return updatedPuzzle;
+      }
+
+      default:
+        return puzzleState;
+    }
+  }
 
   useEffect(() => {
     setBoardBlank(!checkBoardNotBlank(currentPuzzle));
     console.log(boardBlank);
   }, [currentPuzzle, boardBlank]);
 
-  /* ---- Tile Interaction Functions */
-  // R-click to toggle FILL_STATE FILLED / EMPTY
-  const fillTile = (e: React.MouseEvent, rowIndex: number, colIndex: number): void => {
-    setCurrentPuzzle(puzzle => {
-      return puzzle.map((row, i) => {
-        return row.map((fill, j) => {
-          if (rowIndex === i && colIndex === j) {
-            return fill === FILL_STATE.EMPTY ? FILL_STATE.FILLED : FILL_STATE.EMPTY;
-          } else {
-            return fill;
-          }
-        });
-      });
-    });
-  }
-
-  // Add submit button under provider
   return (
     <>
       {!submit && (
         <>
-          <Board currentPuzzle={currentPuzzle} puzzleSolution={[]} livesCount={undefined} fillTile={fillTile} markTile={(e, rowIndex, colIndex) => { }} hoverTile={hoverTile} />
+          <Board currentPuzzle={currentPuzzle} puzzleSolution={[]} livesCount={undefined}
+            fillTile={
+              (e, rowIndex, colIndex) => { currentPuzzleDispatch({ type: PUZZLE_ACTIONS.FILL, rowIndex: rowIndex, colIndex: colIndex }) }
+            }
+            markTile={(e, rowIndex, colIndex) => { }}
+            hoverTile={hoverTile} />
           <button type='button' className='export button' onClick={() => {
             const puzzleCode = createBoolPuzzle(currentPuzzle);
             navigator.clipboard.writeText(puzzleCode).catch((e) => (console.error(e)));
